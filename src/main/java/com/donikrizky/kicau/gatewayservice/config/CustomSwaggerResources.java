98a1ea9ce29448c1384f0b0c8f8e66dbcbe556ac
Doni K Rizky <donikrizky@gmail.com>
2020-09-03 23:37:53 +0700

    update .gitignore

--- src/main/java/com/donikrizky/kicau/gatewayservice/config/CustomSwaggerResources.java
+++ src/main/java/com/donikrizky/kicau/gatewayservice/config/CustomSwaggerResources.java
@@ -17,6 +17,7 @@ public class CustomSwaggerResources implements SwaggerResourcesProvider {
     @Override
     public List<SwaggerResource> get() {
         List<SwaggerResource> resources = new ArrayList<>();
+        resources.add(swaggerResource("auth-service", "/auth/v2/api-docs", "2.0"));
         resources.add(swaggerResource("user-service", "/user/v2/api-docs", "2.0"));
         resources.add(swaggerResource("item-service", "/item/v2/api-docs", "2.0"));
         return resources;
